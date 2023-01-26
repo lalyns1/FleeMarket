cd8be6f2b5b893f41cd1e3f47f6eb768ce6425e5
lalyns <kelwho@naver.com>
2023-01-26 16:00:57 +0900

    #1 [Build] 기초 프로젝트 생성

---
+++ FleeMarket/fleemarket/src/main/java/com/lalyns/fleemarket/FleemarketApplication.java
@@ -0,0 +1,13 @@
+package com.lalyns.fleemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleemarketApplication.class, args);
+	}
+
+}
